23c23e8314b071f096a25c8455c3c715c73d8dc0
yashwanth-git <karthikyashwanth2009@gmail.com>
2023-10-15 17:32:29 -0400

    scraps data using cheerio

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,3 +1,6 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+import { extractPrice, extractCurrency } from "../utils";
 export async function scrapeAmazonProduct(url: string) {
   if (!url) return;
 
@@ -17,6 +20,65 @@ export async function scrapeAmazonProduct(url: string) {
     rejectUnauthorized: false,
   };
   try {
+    const response = await axios.get(url, options);
+    const $ = cheerio.load(response.data);
+
+    const title = $("#productTitle").text().trim();
+    const currentPrice = extractPrice(
+      $(".priceToPay span.a-price-whole"),
+      $("a.size.base.a-color-price"),
+      $(".a-button-selected .a-color-base"),
+      $(".a-price.a-text-price")
+    );
+    const originalPrice = extractPrice(
+      $("#priceblock_ourprice"),
+      $(".a-price.a-text-price span.a-offscreen"),
+      $("#listPrice"),
+      $("#priceblock_dealprice"),
+      $(".a-size-base.a-color-price")
+    );
+
+    const outOfStock =
+      $("#availability span").text().trim().toLowerCase() ===
+      "currently unavailable";
+
+    const images =
+      $("#imgBlkFront").attr("data-a-dynamic-image") ||
+      $("#landingImage").attr("data-a-dynamic-image") ||
+      "{}";
+
+    const imageUrls = Object.keys(JSON.parse(images));
+
+    const currency = extractCurrency($(".a-price-symbol"));
+
+    const discountRate = $(".savingsPercentage").text().replace(/[-%]/g, "");
+
+    console.log({
+      title,
+      currentPrice,
+      originalPrice,
+      outOfStock,
+      imageUrls,
+      currency,
+      discountRate,
+    });
+
+    const data = {
+      url,
+      currency: currency || "$",
+      image: imageUrls[0],
+      title,
+      currentPrice: Number(currentPrice),
+      originalPrice: Number(originalPrice),
+      priceHistory: [],
+      discountRate: Number(discountRate),
+      category: "category", //Future
+      reviewsCount: 100, //Future
+      stars: 4.5, //Future
+      isOutOfStock: outOfStock,
+    };
+
+    console.log(data);
   } catch (error: any) {
     throw new Error(`Failed to scrape product: ${error.message}`);
   }
