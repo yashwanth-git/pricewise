31ef87d93a838f0d9c9e22c990178e7c10be3849
yashwanth-git <karthikyashwanth2009@gmail.com>
2023-10-17 18:35:23 -0400

    creates product in db

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,6 +1,9 @@
 "use server";
+import { revalidatePath } from "next/cache";
 import { scrapeAmazonProduct } from "../scraper";
 import { connectToDB } from "../mongoose";
+import Product from "../models/product.model";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
   if (!productUrl) return;
@@ -8,10 +11,36 @@ export async function scrapeAndStoreProduct(productUrl: string) {
   try {
     connectToDB();
     const scrapedProduct = await scrapeAmazonProduct(productUrl);
+
+    if (!scrapedProduct) return;
+
+    let product = scrapedProduct;
+
+    const existingProduct = await Product.findOne({ url: scrapedProduct.url });
+    if (existingProduct) {
+      const updatedPriceHistory: any = [
+        ...existingProduct.priceHistory,
+        { price: scrapedProduct.currentPrice },
+      ];
+
+      product = {
+        ...scrapedProduct,
+        priceHistory: updatedPriceHistory,
+        lowestPrice: getLowestPrice(updatedPriceHistory),
+        highestPrice: getHighestPrice(updatedPriceHistory),
+        averagePrice: getAveragePrice(updatedPriceHistory),
+      };
+    }
+
+    const newProduct = await Product.findOneAndUpdate(
+      {
+        url: scrapedProduct.url,
+      },
+      product,
+      { upsert: true, new: true }
+    );
     
-    if(!scrapedProduct) return;
-
-
+    revalidatePath(`/products/${newProduct._id}`);
   } catch (err: any) {
     throw new Error(`Failed to create/update product ${err.message}`);
   }
