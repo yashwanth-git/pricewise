373b91a710c020f100dc1b90b306462a244313d1
yashwanth-git <karthikyashwanth2009@gmail.com>
2023-10-24 06:44:54 -0400

    adds cron job

---
+++ app/api/cron/route.ts
@@ -0,0 +1,80 @@
+import Product from "@/lib/models/product.model";
+import { connectToDB } from "@/lib/mongoose";
+import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
+import { scrapeAmazonProduct } from "@/lib/scraper";
+import {
+  getAveragePrice,
+  getEmailNotifType,
+  getHighestPrice,
+  getLowestPrice,
+} from "@/lib/utils";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    connectToDB();
+
+    const products = await Product.find({});
+    if (!products) throw new Error("No products found");
+
+    //Scrape latest product and update it to DB
+    const updatedProducts = await Promise.all(
+      products.map(async (currentProduct) => {
+        const scrapedProduct = await scrapeAmazonProduct(currentProduct.url);
+        if (!scrapedProduct) throw new Error("No product found");
+        const updatedPriceHistory = [
+          ...currentProduct.priceHistory,
+          { price: scrapedProduct.currentPrice },
+        ];
+
+        const product = {
+          ...scrapedProduct,
+          priceHistory: updatedPriceHistory,
+          lowestPrice: getLowestPrice(updatedPriceHistory),
+          highestPrice: getHighestPrice(updatedPriceHistory),
+          averagePrice: getAveragePrice(updatedPriceHistory),
+        };
+
+        const updatedProduct = await Product.findOneAndUpdate(
+          {
+            url: scrapedProduct.url,
+          },
+          product
+        );
+
+        //Check product status and send email
+        const emailNotifType = getEmailNotifType(
+          scrapedProduct,
+          currentProduct
+        );
+
+        if (emailNotifType && updatedProduct.users.length > 0) {
+          const productInfo = {
+            title: updatedProduct.title,
+            url: updatedProduct.url,
+          };
+
+          const emailContent = await generateEmailBody(
+            productInfo,
+            emailNotifType
+          );
+
+          const userEmails = updatedProduct.users.map(
+            (user: any) => user.email
+          );
+
+          await sendEmail(emailContent, userEmails);
+        }
+
+        return updatedProduct;
+      })
+    );
+
+    return NextResponse.json({
+      message: "OK",
+      data: updatedProducts,
+    });
+  } catch (err) {
+    throw new Error(`Error in GET: ${err}`);
+  }
+}
