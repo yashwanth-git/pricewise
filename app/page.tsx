bb7270f085d203be92293f812cc710ff5184f7b3
yashwanth-git <karthikyashwanth2009@gmail.com>
2023-10-26 23:38:13 -0400

    adds console.log

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { getAllProducts } from "@/lib/actions";
 import ProductCard from "@/components/ProductCard";
 const Home = async () => {
   const allProducts = await getAllProducts();
+  console.log(allProducts);
   return (
     <>
       <section className="px-6 mx:px-20 py-24">
@@ -36,7 +37,7 @@ const Home = async () => {
         <h2 className="section-text">Trending</h2>
         <div className="flex flex-wrap gap-x-8 gap-y-16">
           {allProducts?.map((product) => (
-            <ProductCard key={product._id} product={product}/>
+            <ProductCard key={product._id} product={product} />
           ))}
         </div>
       </section>
